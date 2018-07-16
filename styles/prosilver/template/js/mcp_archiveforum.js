01045bbc44a6d8d1a2312a97e25db51a74348269
martti <info@martti.be>
2018-07-16 07:35:28 +0200

    rm style folder

--- styles/prosilver/template/js/mcp_archiveforum.js
+++
@@ -1,6 +0,0 @@
-;(function($, window, document) {
-	$('document').ready(function(){
-		$('div.tabs-container h2')
-			.append($('span.marttiphpbb-pruneinactiveusers'));
-	});
-})(jQuery, window, document);
